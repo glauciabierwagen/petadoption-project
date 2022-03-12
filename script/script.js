d433aad26f03745987b40306f6f1d228aa5d86cf
Jessika <72019869+JessikaGyllstrom@users.noreply.github.com>
2022-03-12 14:05:19 +0100

    adoptioninfo section added

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
+    slidesPerView: 4,
     spaceBetween: 30,
-    slidesPerGroup: 3,
+    slidesPerGroup: 4,
     loop: true,
     loopFillGroupWithBlank: true,
     pagination: {
