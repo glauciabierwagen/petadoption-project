dd6b30851273eeb120c90a5226644d6877eb23a3
Jessika <72019869+JessikaGyllstrom@users.noreply.github.com>
2022-03-18 10:26:40 +0100

    responsive mobilenav added

--- script/script.js
+++ script/script.js
@@ -1,3 +1,11 @@
+const hamburger = document.querySelector(".hamburger");
+const mobile_menu = document.querySelector(".mobile-nav");
+
+hamburger.addEventListener("click", function () {
+    hamburger.classList.toggle("is-active");
+    mobile_menu.classList.toggle("is-active");
+});
+
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 4,
     spaceBetween: 30,
